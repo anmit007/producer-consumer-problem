b110e21dd13d17bda52b72bf2baebc6c32f62777
AnmZ <anmit.ray911@gmail.com>
2023-09-09 17:48:08 +0530

    setup basic guidelines

--- main.go
+++ main.go
@@ -2,4 +2,16 @@ package main
 
 func main() {
 
+	//seed the random number generator
+
+	// print out a starting message
+
+	// create a producer
+
+	// run the producer in the background
+
+	// create and run consumer
+
+	// print out a ending message
+
 }
